0e9491afad735ab358cf930bdc2ad2b9ba39c358
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-09-03 22:53:18 -0300

    feat: create base user_services

---
+++ user_service/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db.init_app(app)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_dict())
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.json
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict()), 201
+
+if __name__ == '__main__':
+    app.run(port=5003)
